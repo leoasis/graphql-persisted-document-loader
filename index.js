8cc17d3260b6ef64ac0e781841ed42a6743e690f
kbariotis <konmpar@gmail.com>
2020-12-04 21:01:47 +0200

    replace persistedgraphql with latest graphql/apollo-utils

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const vm = require('vm');
-const os = require('os');
-const loaderUtils = require('loader-utils');
-const { ExtractGQL } = require('persistgraphql/lib/src/ExtractGQL');
-const queryTransformers = require('persistgraphql/lib/src/queryTransformers');
+const vm = require("vm");
+const os = require("os");
+const loaderUtils = require("loader-utils");
 
 module.exports = function graphQLPersistedDocumentLoader(content) {
   const deps = [];
@@ -13,20 +11,22 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
   // executing the generated code from the graphql-tag loader.
   const sandbox = {
     require(file) {
-      deps.push(new Promise((resolve, reject) => {
-        context.loadModule(file, (err, source, sourceMap, module) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({ source, sourceMap, module });
-          }
-        });
-      }));
+      deps.push(
+        new Promise((resolve, reject) => {
+          context.loadModule(file, (err, source, sourceMap, module) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve({ source, sourceMap, module });
+            }
+          });
+        })
+      );
       return { definitions: [] };
     },
     module: {
-      exports: null
-    }
+      exports: null,
+    },
   };
   // Run the graphql-tag loader generated code to capture the dependencies.
   vm.runInNewContext(content, sandbox);
@@ -41,7 +41,11 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
     // If no deps, just try to generate the document id from the
     // source returned from graphql-tag loader result. This will
     // add an id only if the source contains an operation.
-    content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
+    content = tryAddDocumentId(
+      options,
+      content,
+      this._module._graphQLQuerySource
+    );
     return content;
   }
 
@@ -50,54 +54,55 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
   // If we have dependencies, we retrieve their query sources and
   // concatenate them with the one from this module, to create the
   // full query source.
-  Promise.all(deps).then((modules) => {
-    modules.forEach((mod, index) => {
-      this._module._graphQLQuerySource += mod.module._graphQLQuerySource;
+  Promise.all(deps)
+    .then((modules) => {
+      modules.forEach((mod, index) => {
+        this._module._graphQLQuerySource += mod.module._graphQLQuerySource;
+      });
+
+      try {
+        // Now that we have all our dependencies' sources concatenated
+        // with this module's query source, we can send all that to
+        // generate the document id, if the resulting source
+        // is for an operation.
+        content = tryAddDocumentId(
+          options,
+          content,
+          this._module._graphQLQuerySource
+        );
+      } catch (e) {
+        callback(e);
+      }
+
+      callback(null, content);
+    })
+    .catch((err) => {
+      console.log("error", err);
+      callback(err);
     });
-
-    try {
-      // Now that we have all our dependencies' sources concatenated
-      // with this module's query source, we can send all that to
-      // generate the document id, if the resulting source
-      // is for an operation.
-      content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
-    } catch (e) {
-      callback(e);
-    }
-
-    callback(null, content);
-  }).catch((err) => {
-    console.log('error', err);
-    callback(err);
-  });
 };
 
 function tryAddDocumentId(options, content, querySource) {
-  const queryMap = new ExtractGQL({
-    queryTransformers: [options.addTypename && queryTransformers.addTypenameTransformer].filter(Boolean)
-  }).createOutputMapFromString(querySource);
+  const operations = separateOperations(parse(querySource));
 
-  const queries = Object.keys(queryMap);
-  if (queries.length > 1) {
-    queries
-      .map(query => {
-        const matched = query.match(/^(mutation|query)\ ([^\ \(\{]*)/)
-        if (!matched) {
-          return false
-        }
-        return {
-          operationName: matched[2],
-          id: generateIdForQuery(options, query)
-        }
-      })
-      .filter(isValid => !!isValid)
-      .forEach(({ id, operationName }) => {
-        content += `${os.EOL}module.exports["${operationName}"].documentId = ${JSON.stringify(id)};`
-      })
-  } else if (queries.length === 1) {
-    const queryId = generateIdForQuery(options, Object.keys(queryMap)[0]);
-    content += `${os.EOL}doc.documentId = ${JSON.stringify(queryId)}`;
-  }
+  Object.keys(operations).map((operation) => {
+    const document = options.addTypename
+      ? addTypenameToDocument(operations[operation])
+      : operations[operation];
+    const query = print(document);
+
+    const queryId = generateIdForQuery(options, query);
+
+    // Add them as exports to the final file
+    // If there is only one operation, it will be the default export
+    if (operations.length > 1) {
+      content += `${
+        os.EOL
+      }module.exports["${operation}"].documentId = ${JSON.stringify(queryId)};`;
+    } else {
+      content += `${os.EOL}doc.documentId = ${JSON.stringify(queryId)}`;
+    }
+  });
 
   return content;
 }
@@ -105,5 +110,5 @@ function tryAddDocumentId(options, content, querySource) {
 function generateIdForQuery(options, query) {
   if (options.generateId) return options.generateId(query);
 
-  return require('crypto').createHash('sha256').update(query).digest('hex');
+  return require("crypto").createHash("sha256").update(query).digest("hex");
 }
