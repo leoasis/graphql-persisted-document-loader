f88e805da09afc8bc254f28520030daf7793fdeb
kbariotis <konmpar@gmail.com>
2020-12-04 21:10:17 +0200

    remove formatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const vm = require("vm");
-const os = require("os");
-const loaderUtils = require("loader-utils");
+const vm = require('vm');
+const os = require('os');
+const loaderUtils = require('loader-utils');
 
 module.exports = function graphQLPersistedDocumentLoader(content) {
   const deps = [];
@@ -11,22 +11,20 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
   // executing the generated code from the graphql-tag loader.
   const sandbox = {
     require(file) {
-      deps.push(
-        new Promise((resolve, reject) => {
-          context.loadModule(file, (err, source, sourceMap, module) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve({ source, sourceMap, module });
-            }
-          });
-        })
-      );
+      deps.push(new Promise((resolve, reject) => {
+        context.loadModule(file, (err, source, sourceMap, module) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({ source, sourceMap, module });
+          }
+        });
+      }));
       return { definitions: [] };
     },
     module: {
-      exports: null,
-    },
+      exports: null
+    }
   };
   // Run the graphql-tag loader generated code to capture the dependencies.
   vm.runInNewContext(content, sandbox);
@@ -41,11 +39,7 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
     // If no deps, just try to generate the document id from the
     // source returned from graphql-tag loader result. This will
     // add an id only if the source contains an operation.
-    content = tryAddDocumentId(
-      options,
-      content,
-      this._module._graphQLQuerySource
-    );
+    content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
     return content;
   }
 
@@ -54,32 +48,26 @@ module.exports = function graphQLPersistedDocumentLoader(content) {
   // If we have dependencies, we retrieve their query sources and
   // concatenate them with the one from this module, to create the
   // full query source.
-  Promise.all(deps)
-    .then((modules) => {
-      modules.forEach((mod, index) => {
-        this._module._graphQLQuerySource += mod.module._graphQLQuerySource;
-      });
-
-      try {
-        // Now that we have all our dependencies' sources concatenated
-        // with this module's query source, we can send all that to
-        // generate the document id, if the resulting source
-        // is for an operation.
-        content = tryAddDocumentId(
-          options,
-          content,
-          this._module._graphQLQuerySource
-        );
-      } catch (e) {
-        callback(e);
-      }
-
-      callback(null, content);
-    })
-    .catch((err) => {
-      console.log("error", err);
-      callback(err);
+  Promise.all(deps).then((modules) => {
+    modules.forEach((mod, index) => {
+      this._module._graphQLQuerySource += mod.module._graphQLQuerySource;
     });
+
+    try {
+      // Now that we have all our dependencies' sources concatenated
+      // with this module's query source, we can send all that to
+      // generate the document id, if the resulting source
+      // is for an operation.
+      content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
+    } catch (e) {
+      callback(e);
+    }
+
+    callback(null, content);
+  }).catch((err) => {
+    console.log('error', err);
+    callback(err);
+  });
 };
 
 function tryAddDocumentId(options, content, querySource) {
@@ -110,5 +98,5 @@ function tryAddDocumentId(options, content, querySource) {
 function generateIdForQuery(options, query) {
   if (options.generateId) return options.generateId(query);
 
-  return require("crypto").createHash("sha256").update(query).digest("hex");
+  return require('crypto').createHash('sha256').update(query).digest('hex');
 }
